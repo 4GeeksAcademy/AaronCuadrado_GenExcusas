d445e3e8609861335719e01e13ad5034b6e04007
AaronCuadrado <aaroncuadradotoral@gmail.com>
2024-07-08 11:52:00 +0200

    fin

--- src/app.js
+++ src/app.js
@@ -1,11 +1,21 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = function () {
+  console.log("iniciando");
+  document.getElementById("genExcusa").addEventListener("click", () => {
+    console.log("click");
+    document.getElementById("excusa").innerHTML = generadorexcusas();
+  });
 };
+
+const quien = ["Mi gato diabético", "Mi ex que todavia me acosa", "El vecino que no me cae bien", "El señor de 95 años"];
+const accion = ["se comió", "vomitó sobre", "se cayó encima de", "destruyó"];
+const que = ["mis deberes", "mis llaves del coche", "mi cena", "mi ordenador"];
+const cuando = ["justo antes de salir de casa", "mientras hacía una videollamada importante", "cuando estaba a punto de dormir", "durante el funeral de mi pez"];
+
+function generadorexcusas() {
+  let randomquien = quien[Math.floor(Math.random() * quien.length)];
+  let randomaccion = accion[Math.floor(Math.random() * accion.length)];
+  let randomque = que[Math.floor(Math.random() * que.length)];
+  let randomcuando = cuando[Math.floor(Math.random() * cuando.length)];
+  const excusadiferente = `${randomquien} ${randomaccion} ${randomque} ${randomcuando}`;
+  return excusadiferente;
+}
